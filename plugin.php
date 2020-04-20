a7206d16ff2ffddb725a63fdadec124e1ac808d0
Stephen Dickinson <stephencottontail@me.com>
2020-04-19 18:17:35 -0600

    Set up the block on the front-end

--- plugin.php
+++ plugin.php
@@ -11,6 +11,20 @@
 		public $editor_script = 'sc-recent-posts-editor-script';
 		public $editor_style = 'sc-recent-posts-editor-style';
 		public $render_callback = 'sc_recent_posts_render';
+		public $attributes = array(
+			'backgroundImage' => array(
+				'type' => 'string',
+				'default' => ''
+			),
+			'selectedType' => array(
+				'type' => 'string',
+				'default' => 'post'
+			),
+			'postsPerBlock' => array(
+				'type' => 'integer',
+				'default' => 5
+			)
+		);
 
 		public function __construct() {
 			parent::__construct( $this->name, array(
@@ -25,14 +39,70 @@
 	}
 
 	function sc_recent_posts_render( $attributes ) {
-		return '<p>Hello from PHP!</p>';
+		$background_image = $attributes['backgroundImage'];
+		$selected_type = $attributes['selectedType'];
+		$posts_per_block = $attributes['postsPerBlock'];
+
+		$recent_posts_query = new WP_Query( array(
+			'post_type' => $selected_type,
+			'posts_per_page' => $posts_per_block,
+		) );
+
+		if ( $recent_posts_query->have_posts() ) :
+			ob_start(); ?>
+
+			<div class="wp-block-sc-recent-posts">
+				<div class="wp-block-sc-recent-posts__windows" style="<?php printf( 'background-image: url(%s);', $background_image ); ?>">
+					<div class="wp-block-sc-recent-posts__window">
+						<div class="top-shade"></div>
+						<div class="bottom-shade"></div>
+					</div>
+					<div class="wp-block-sc-recent-posts__window">
+						<div class="top-shade"></div>
+						<div class="bottom-shade"></div>
+					</div>
+					<div class="wp-block-sc-recent-posts__window">
+						<div class="top-shade"></div>
+						<div class="bottom-shade"></div>
+					</div>
+					<div class="wp-block-sc-recent-posts__door"></div>
+				</div><!-- .wp-block-sc-recent-posts__windows -->
+				<?php
+					printf( '<div class="wp-block-sc-recent-posts__jsx"><code class="jsx">&lt;<span class="jsx-tag">%s</span> <span class="jsx-attr">type</span>="<span class="jsx-attr-string">%s</span>" <span class="jsx-attr">number</span>="<span class="jsx-attr-string">%s</span>"&gt;</code></div>',
+							'RecentPosts',
+							$selected_type,
+							$posts_per_block
+					);
+				?>
+				<div class="wp-block-sc-recent-posts__entries">
+					<?php
+						while ( $recent_posts_query->have_posts() ) {
+							$recent_posts_query->the_post();
+
+							printf( '<article class="wp-block-sc-recent-posts__entry"><h2 class="wp-block-sc-recent-posts__title"><a href="%s">%s</a></h2><time datetime="%s">%s</time></article>',
+									esc_url( get_the_permalink() ),
+									esc_html( get_the_title() ),
+									esc_attr( get_the_time( 'C' ) ),
+									esc_html( get_the_date() )
+							);
+						}
+					?>
+				</div><!-- .wp-block-sc-recent-posts__entries -->
+			</div><!-- .wp-block-sc-recent-posts -->
+
+	<?php
+		endif;
+			
+		wp_reset_postdata();
+		wp_reset_query();
+		
+		return ob_get_clean();
 	}
 
 	add_action( 'init', function() {
 		$script_path = 'js/block.js';
 		$script_url = plugins_url( $script_path, __FILE__ );
 
-		wp_register_style( 'sc-recent-posts-editor-style', plugins_url( 'js/editor.css', __FILE__ ), array( 'wp-components' ) );
 		wp_register_script( 'sc-recent-posts-editor-script', $script_url, array( 'wp-block-editor', 'wp-components', 'wp-blocks', 'wp-data', 'wp-element', 'moment' ) );
 
 		$recent_posts = new SC_Recent_Posts();
